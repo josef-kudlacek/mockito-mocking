58df10af6704ec69e9aa25bce9922f453107bfb0
Josef Kudláček <j.k.kudlacek@gmail.com>
2024-07-26 09:23:35 +0200

    Mocking with Mockito - when and verify

--- src/test/java/com/luv2code/test/MockAnnotationTest.java
+++ src/test/java/com/luv2code/test/MockAnnotationTest.java
@@ -6,11 +6,18 @@ import com.luv2code.component.models.CollegeStudent;
 import com.luv2code.component.models.StudentGrades;
 import com.luv2code.component.service.ApplicationService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(classes = MvcTestingExampleApplication.class)
 public class MockAnnotationTest {
@@ -36,6 +43,19 @@ public class MockAnnotationTest {
         collegeStudent.setLastname("Kudláček");
         collegeStudent.setEmailAddress("joseph.kudlacek@gmail.com");
         collegeStudent.setStudentGrades(studentGrades);
+    }
+
+    @DisplayName("When & Verify")
+    @Test
+    public void assertEqualsTestAddGrades() {
+        when(applicationDao.addGradeResultsForSingleClass(studentGrades.getMathGradeResults()))
+                .thenReturn(100.00);
+
+        assertEquals(100, applicationService.addGradeResultsForSingleClass(
+                collegeStudent.getStudentGrades().getMathGradeResults()));
+
+        Mockito.verify(applicationDao, times(1)).addGradeResultsForSingleClass(
+                studentGrades.getMathGradeResults());
     }
 
 }
